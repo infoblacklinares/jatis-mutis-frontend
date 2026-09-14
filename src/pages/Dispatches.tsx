7438fdf0c15af4b58ffb05e3859bc2429273cb7e
infoblacklinares <infoblack.linares@gmail.com>
2026-09-13 23:43:08 -0300

    fix: use correct Clerk React package in dispatches

--- src/pages/Dispatches.tsx
+++ src/pages/Dispatches.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import { useUser } from "@clerk/clerk-react";
+import { useUser } from "@clerk/react";
 import { getOrders, updateOrder } from "../services/api";
 import type { Order } from "../types/order";
 import { readOrders, saveOrders, logActivity } from "../services/localStore";
